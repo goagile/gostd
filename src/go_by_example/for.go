0b2492536f182e3e0124a8abb1573982113754a1
khardi <kharchilindanila@gmail.com>
2018-01-30 11:15:33 +0300

    static

---
+++ src/go_by_example/for.go
@@ -0,0 +1,18 @@
+package main
+
+
+import "fmt"
+
+
+func main() {
+
+	fmt.Println("While loop:")
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	// fmt.Println("Hello ")
+
+}
---
+++ src/go_by_example/variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var s string = "Hello"
+	fmt.Println(s)
+
+	var x, y int = 2, 3
+	fmt.Println(x, y)
+
+	var e int
+	fmt.Println(e)
+
+}
