ceb2ea5d53b69ee7255ceee068ae4b57ac29496b
khardi <kharchilindanila@gmail.com>
2020-07-30 23:57:13 +0300

    a

---
+++ operators/for_optional/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := 1
+	for s < 18 {
+		s ++
+	}
+	fmt.Println(s)
+}
---
+++ operators/swith/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 2
+	y := 2
+
+	switch {
+	
+	case x == 1:
+		fmt.Println("X One")
+	
+	case y == 2:
+		fmt.Println("Y Two")
+		
+	}
+}
