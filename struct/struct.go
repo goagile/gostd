0fb694678cc75e913f3229ce6b0be57c2648989f
khardi <kharchilindanila@gmail.com>
2020-08-07 21:56:45 +0300

    struct

---
+++ struct/struct.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+func (p Point) Abs() float64 {
+	return math.Sqrt(p.X * p.X + p.Y * p.Y)
+}
+
+func (p Point) ShiftX(dx float64) {
+	p.X += dx
+}
+
+func abs(p Point) float64 {
+	return math.Sqrt(p.X * p.X + p.Y * p.Y)
+}
+
+func shiftx(p Point, dx float64) {
+	p.X += dx
+}
+
+func main() {
+	p := Point{4, 5}
+	fmt.Println(p.Abs())
+	p.ShiftX(10)
+	fmt.Println(p.Abs())
+
+	b := Point{4, 5}
+	fmt.Println(abs(b))
+	shiftx(b, 10)
+	fmt.Println(abs(b))
+}
