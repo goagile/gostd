c21066f3503e6e88897602489697a7bbea0cc27a
khardi <kharchilindanila@gmail.com>
2020-08-08 23:40:22 +0300

    types

--- type/example.go
+++ type/example.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
@@ -27,6 +28,10 @@ func main() {
 
 
 	fmt.Println()
+	fmt.Println("byte 255 is ", what(byte(255)))
+
+
+	fmt.Println()
 	fmt.Println(what(int8(-128)))
 	fmt.Println(what(int8( 127)))
 
@@ -46,8 +51,33 @@ func main() {
 	fmt.Println(what(int(-9223372036854775808)))
 	fmt.Println(what(int( 9223372036854775807)))
 
+
+	fmt.Println()
+	fmt.Println("math.MaxFloat32 is ", math.MaxFloat32)
+	fmt.Println(what(float32(math.MaxFloat32)))
+
+	fmt.Println()
+	fmt.Println("math.MaxFloat64 is ", math.MaxFloat64)
+	fmt.Println(what(float64(math.MaxFloat64)))
+
+
+	fmt.Println()
+	fmt.Println(what(complex(4, 5)))
+
+	fmt.Println()
+	fmt.Println(what(complex(math.MaxFloat64, math.MaxFloat64)))
+
+
 	fmt.Println()
 	fmt.Println(what(true))
+
+
+	fmt.Println()
+	fmt.Println("rune 'a' is ", what(rune('a')))
+
+	fmt.Println()
+	fmt.Println(what("Hello"))
+
 }
 
 func what(v interface{}) string {
@@ -73,6 +103,7 @@ func what(v interface{}) string {
 	case uintptr:
 		return fmt.Sprintf("Unsigned Integer Pointer: %v", v)
 
+
 	case int8:
 		return fmt.Sprintf("Integer 8 bit: %v", v)
 
@@ -89,8 +120,27 @@ func what(v interface{}) string {
 		return fmt.Sprintf("Integer: %v", v)
 
 
+	case float32:
+		return fmt.Sprintf("Float32: %v", v)
+
+	case float64:
+		return fmt.Sprintf("Float64: %v", v)
+
+
+	case complex64:
+		return fmt.Sprintf("complex64: %v", v)
+
+	case complex128:
+		return fmt.Sprintf("complex128: %v", v)
+
+
 	case bool:
 		return fmt.Sprintf("Boolean: %v", v)	
+
+
+	case string:
+		return fmt.Sprintf("String: %v", v)	
+
 
 	default:
 		return "Unknown type"
