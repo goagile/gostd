55373f602fafde8e5e24b08f8f9f800879031507
khardi <kharchilindanila@gmail.com>
2020-08-11 13:59:41 +0300

    stream reader

--- json/stream/stream.go
+++ json/stream/stream.go
@@ -3,19 +3,98 @@ package main
 import (
 	"fmt"
 	"os"
-	"io/ioutil"
+	// "io/ioutil"
 	"log"
+	"encoding/json"
 )
 
 func main() {
-	f, err := os.Open("./test/example.json")
+
+	r, err := NewPersonReader("./test/example.json")
 	if err != nil {
-		log.Fatalf("Open: %v\n", err)
+		log.Fatalf("NewPersonReader %v\n", err)
 	}
-	b, err := ioutil.ReadAll(f)
+
+	for r.NextPerson() {
+		p, err := r.ScanPerson()
+		if err != nil {
+			log.Fatalf("ScanPerson err: %v\n", err)
+		}
+		fmt.Println(p)
+	}
+
+	fmt.Printf("Read %v persons.\n", r.Count())
+
+}
+
+func NewPersonReader(filename string) (*PersonReader, error) {
+	f, err := os.Open(filename)
 	if err != nil {
-		log.Fatalf("ReadAll: %v\n", err)
+		return nil, err
 	}
-	defer f.Close()
-	fmt.Println(string(b))
+	r := &PersonReader{
+		dec: json.NewDecoder(f),
+		next: true,
+		scrolled: false,
+	}
+	return r, nil
+}
+
+type PersonReader struct {
+	dec *json.Decoder
+	count int
+	file *os.File
+	p *Person
+	err error
+	next bool
+	scrolled bool
+}
+
+func (r *PersonReader) Count() int {
+	return r.count
+}
+
+func (r *PersonReader) ScanPerson() (*Person, error) {
+	return r.p, r.err
+}
+
+func (r *PersonReader) NextPerson() bool {
+	defer func() {
+		if r.next == false {
+			r.file.Close()
+		}
+	}()
+
+	if !r.scrolled {
+		for r.dec.More() {
+			t, err := r.dec.Token()
+			if err != nil {
+				r.next = false
+				return r.next
+			}
+			if t == "persons" {
+				fmt.Printf("%T %v\n", t, t)
+				if _, err := r.dec.Token(); err != nil {
+					r.next = false
+					return r.next
+				}
+				break;
+			}
+		}
+		r.scrolled = true
+	}
+
+	if err := r.dec.Decode(&r.p); err != nil {
+		r.next = false
+		return r.next
+	}
+	r.count++
+
+	r.next = true
+	return r.next
+}
+
+type Person struct {
+	Name string `json:"name"`
+	Age float64 `json:"age"`
 }
