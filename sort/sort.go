9ff39d705e4a537947156be1756fc4494e58e9f2
khardi <kharchilindanila@gmail.com>
2020-10-22 19:44:25 +0300

    sort

--- sort/sort.go
+++ sort/sort.go
@@ -22,4 +22,39 @@ func main() {
 	r := []int{2, 3, 4, 5}
 	sort.Sort(sort.Reverse(sort.IntSlice(r)))
 	fmt.Println(r)
+
+	runes := []rune{'g', 'o', 'o', 'g', 'l', 'e'}
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+	for _, r := range runes {
+		fmt.Printf("%v ", string(r))
+	}
+	fmt.Println()
+
+	students := []*Student{&Student{21}, &Student{19}, &Student{20}}
+	sort.Sort(ByAge(students))
+	fmt.Println(students)
+}
+
+type Student struct {
+	Age int
+}
+
+func (s *Student) String() string {
+	return fmt.Sprintf("Student(%v)", s.Age)
+}
+
+type ByAge []*Student
+
+func (a ByAge) Less(i, j int) bool {
+	return a[i].Age < a[j].Age
+}
+
+func (a ByAge) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByAge) Len() int {
+	return len(a)
 }
