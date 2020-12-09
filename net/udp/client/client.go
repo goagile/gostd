19740821b23f17a9b15fb2c0d43b321880e742d5
khardi <kharchilindanila@gmail.com>
2020-12-09 18:38:26 +0300

    udp

--- net/udp/client/client.go
+++ net/udp/client/client.go
@@ -2,108 +2,70 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net"
 	"os"
-	"strings"
 )
 
-func main() {
-	addr := address()
-	log.Println("UDP Client: ", addr)
-
-	udp := resolve(addr)
-	conn := client(udp)
-	defer conn.Close()
-
-	go readLoop(conn)
-	writeLoop(conn)
+func init() {
+	log.SetFlags(log.Lshortfile)
 }
 
-func address() string {
+func main() {
 	args := os.Args[1:]
 	if len(args) < 2 {
-		log.Fatalln("usage: ./server host port")
+		log.Fatalln("usage: ./client host port")
 	}
+	a := net.JoinHostPort(args[0], args[1])
 
-	return net.JoinHostPort(args[0], args[1])
-}
-
-func resolve(addr string) *net.UDPAddr {
-	udpa, err := net.ResolveUDPAddr("udp4", addr)
+	raddr, err := net.ResolveUDPAddr("udp4", a)
 	if err != nil {
-		log.Println("ResolveUDPAddr", err)
+		log.Fatalln("ResolveUDPAddr", err)
 	}
 
-	return udpa
-}
-
-func client(udpa *net.UDPAddr) *net.UDPConn {
-	conn, err := net.DialUDP("udp4", nil, udpa)
+	c, err := net.DialUDP("udp4", nil, raddr)
 	if err != nil {
-		log.Println("DialUDP", err)
+		log.Fatalln("DialUDP", err)
 	}
+	defer c.Close()
 
-	return conn
+	go readLoop(c)
+
+	writeLoop(c)
 }
 
-func writeLoop(conn *net.UDPConn) {
+func readLoop(c *net.UDPConn) {
 	for {
-		if err := write(conn); err != nil {
-			log.Println(err)
+		b := make([]byte, 1024)
+		n, addr, err := c.ReadFromUDP(b)
+		if err != nil {
+			log.Println("ReadFromUDP", err)
 			continue
 		}
+		fmt.Printf("\n<-- read %v bytes from server %v: %v\n", n, addr, string(b))
 	}
 }
 
-func readLoop(conn *net.UDPConn) {
+func writeLoop(c *net.UDPConn) {
 	for {
-		if err := read(conn); err != nil {
-			log.Println(err)
+		fmt.Print(">>> ")
+		b, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
+		if err != nil {
+			log.Println("ReadBytes", err)
 			continue
 		}
+		b = bytes.Trim(b, "\n")
+		if len(b) == 0 {
+			continue
+		}
+		n, err := c.Write(b)
+		if err != nil {
+			log.Println("Write", err)
+			continue
+		}
+		a := c.RemoteAddr().String()
+		fmt.Printf("--> send %v bytes to server %v: %v\n", n, a, string(b))
 	}
-}
-
-func write(conn *net.UDPConn) error {
-	fmt.Print(">> ")
-
-	r := bufio.NewReader(os.Stdin)
-	b, err := r.ReadBytes('\n')
-	if err != nil {
-		return fmt.Errorf("ReadString %v", err)
-	}
-	quit(b)
-
-	b = append(b, "\n"...)
-	n, err := conn.Write(b)
-	if err != nil {
-		return fmt.Errorf("Write %v", err)
-	}
-	log.Println("Write ", n, " bytes")
-
-	return nil
-}
-
-func quit(b []byte) {
-	switch strings.TrimSpace(string(b)) {
-	case "q", "quit":
-		fmt.Println("Bye!")
-		os.Exit(0)
-	}
-}
-
-func read(conn *net.UDPConn) error {
-	b := make([]byte, 1024)
-
-	n, addr, err := conn.ReadFromUDP(b)
-	if err != nil {
-		return fmt.Errorf("ReadFromUDP %v %v", addr, err)
-	}
-
-	log.Println("Read ", n, " bytes from ", addr)
-	log.Println(string(b))
-
-	return nil
 }
--- net/udp/server/server.go
+++ net/udp/server/server.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 func init() {
@@ -12,83 +13,42 @@ func init() {
 }
 
 func main() {
-	addr := address()
-	log.Println("UDP Server: ", addr)
-
-	udp := resolve(addr)
-	conn := server(udp)
-	defer conn.Close()
-
-	go readLoop(conn)
-	writeLoop(conn)
-}
-
-func address() string {
 	args := os.Args[1:]
 	if len(args) < 2 {
 		log.Fatalln("usage: ./server host port")
 	}
+	a := net.JoinHostPort(args[0], args[1])
+	fmt.Println("udp server listen: ", a)
 
-	return net.JoinHostPort(args[0], args[1])
-}
-
-func resolve(addr string) *net.UDPAddr {
-	udpa, err := net.ResolveUDPAddr("udp4", addr)
+	laddr, err := net.ResolveUDPAddr("udp4", a)
 	if err != nil {
-		log.Println("ResolveUDPAddr", err)
+		log.Fatalln("ResolveUDPAddr", err)
 	}
 
-	return udpa
-}
-
-func server(udpa *net.UDPAddr) *net.UDPConn {
-	conn, err := net.ListenUDP("udp4", udpa)
+	c, err := net.ListenUDP("udp4", laddr)
 	if err != nil {
-		log.Println("DialUDP", err)
+		log.Fatalln("ListenUDP", err)
 	}
+	defer c.Close()
 
-	return conn
-}
-
-func writeLoop(conn *net.UDPConn) {
 	for {
-		if err := write(conn); err != nil {
-			log.Println(err)
+		b := make([]byte, 1024)
+		n, addr, err := c.ReadFromUDP(b)
+		if err != nil {
+			log.Println("ReadFromUDP", err)
 			continue
 		}
-	}
-}
+		fmt.Printf("\n<-- read %v bytes from client %v: %v\n", n, addr, string(b))
 
-func readLoop(conn *net.UDPConn) {
-	for {
-		if err := read(conn); err != nil {
-			log.Println(err)
+		fmt.Println("!!! start ... ")
+		time.Sleep(3 * time.Second)
+		fmt.Println("!!! end ... ")
+
+		m, err := c.WriteToUDP(b, addr)
+		if err != nil {
+			log.Println("WriteToUDP", err)
 			continue
 		}
+		fmt.Printf("--> send %v bytes to client %v: %v", m, addr, string(b))
 	}
-}
-
-func write(conn *net.UDPConn) error {
-	b := []byte("TIME")
-	b = append(b, "\n"...)
-	n, err := conn.WriteToUDP(b, addr)
-	if err != nil {
-		return fmt.Errorf("Write %v", err)
-	}
-	log.Println("Write ", n, " bytes")
-	return nil
-}
-
-func read(conn *net.UDPConn) error {
-	b := make([]byte, 1024)
-
-	n, addr, err := conn.ReadFromUDP(b)
-	if err != nil {
-		return fmt.Errorf("ReadFromUDP %v %v", addr, err)
-	}
-
-	log.Println("Read ", n, " bytes from ", addr)
-	log.Println(string(b))
-
-	return nil
 }
