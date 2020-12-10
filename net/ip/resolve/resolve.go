33370ac870a3a9a883468c2d4e808670d0993a1b
khardi <kharchilindanila@gmail.com>
2020-12-10 13:15:42 +0300

    resolve ip

---
+++ net/ip/resolve/resolve.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) < 1 {
+		fmt.Fprintln(os.Stderr, "usage: ./resolve hostname")
+		fmt.Fprintln(os.Stderr, "example: ./resolve google.ru")
+		os.Exit(1)
+	}
+	host := args[0]
+	ip, err := net.ResolveIPAddr("ip", host)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "cannot resolve ip for host", host)
+		os.Exit(1)
+	}
+	fmt.Println("resolved ip: ", ip, " for host", host)
+}
